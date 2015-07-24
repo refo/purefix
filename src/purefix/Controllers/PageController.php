48d792db5e91da417725ae8bd2439659c7828d71
Refik ONAL <refikonal@gmail.com>
2015-07-24 23:45:53 +0300

    Clear "page" controller

--- src/purefix/Controllers/PageController.php
+++ src/purefix/Controllers/PageController.php
@@ -11,26 +11,6 @@ class PageController extends Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->meta['titlePrefix'] = 'Sayfa: ';
     }
-
-    public function getTestCart()
-    {
-
-        Cart::add([
-            'id' => 'ABC123',
-            'name' => 'Juliet',
-            'qty' => 1,
-            'price' => 329,
-            'options' => ['size' => 'Medium - 56cm']
-        ]);
-
-        return Cart::content();
-    }
-
-    public function getTest()
-    {
-        $this->meta['title'] = 'TEST';
-        return $this->createResponse('content/lorem', []);
-    }
+    
 }

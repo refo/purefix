495a4aafbafaa7f5dd1606bf429b23fdecb646f5
Refik ONAL <refikonal@gmail.com>
2015-07-24 22:35:55 +0300

    Add CUSTOMER_PATH constant

--- src/boot.php
+++ src/boot.php
@@ -1,12 +1,19 @@
 <?php
 
+// Customer constants
+define('CUSTOMER_PATH', realpath(__DIR__.'/purefix'));
+
+// Composer autoloader
 $loader = require __DIR__.'/vendor/autoload.php';
 
+// Laravel applicaiton
 $app = require_once __DIR__.'/laravel-master'.'/bootstrap/app.php';
+
 
 // Run The Application
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()

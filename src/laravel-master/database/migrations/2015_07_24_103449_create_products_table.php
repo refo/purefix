933583649f36e583c57f663a0f4b8b08c72c3c75
Refik ONAL <refikonal@gmail.com>
2015-07-27 09:19:30 +0300

    Change migration for testing purposes

--- src/laravel-master/database/migrations/2015_07_24_103449_create_products_table.php
+++ src/laravel-master/database/migrations/2015_07_24_103449_create_products_table.php
@@ -14,16 +14,16 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('slug');
-            $table->string('tags');
+            // $table->string('slug');
+            // $table->string('tags');
             $table->string('title');
-            $table->string('body');
-            $table->string('vendor');
-            $table->string('product-type');
-            $table->string('template');
-            $table->string('options');
-            $table->string('images');
-            $table->string('image');
+            // $table->string('body');
+            // $table->string('vendor');
+            // $table->string('product-type');
+            // $table->string('template');
+            // $table->string('options');
+            // $table->string('images');
+            // $table->string('image');
 
             $table->timestamps();
         });

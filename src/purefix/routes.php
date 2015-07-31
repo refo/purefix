a2c5d47e847df267f8f2e4a3b30a1f203aef659c
Refik ONAL <refikonal@gmail.com>
2015-07-31 11:48:12 +0300

    Add /products route

--- src/purefix/routes.php
+++ src/purefix/routes.php
@@ -6,6 +6,8 @@ Route::get('test', function(){
     return 'TEST OK';
 });
 
+Route::get('products', 'ProductController@all');
+
 Route::controller('page', 'PageController');
 
 Route::controller('test', 'TestController');

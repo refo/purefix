61e02c9a04a53b884db7a7b71f95652de47958e7
Refik ONAL <refikonal@gmail.com>
2015-07-31 21:29:17 +0300

    Should invoke parent constructor / Add detail method

--- src/purefix/Controllers/ProductController.php
+++ src/purefix/Controllers/ProductController.php
@@ -7,9 +7,24 @@ use Purefix\Models\ProductVariant;
 
 class ProductController extends Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function all()
     {
         return Product::with('variants')->get();
+    }
+
+    public function detail($slug)
+    {
+        $product = Product::with('variants')->where('slug', $slug)->first();
+        $product->url = route('product', $product->slug);
+
+        $view = hbs('content/product-detail', $product);
+
+        return $this->layout($view);
     }
 
 

df119f5d7876874cc0f8617b18cffe025714ab12
Refik ONAL <refikonal@gmail.com>
2015-08-22 20:42:09 +0300

    Add n11 link toggling feature

---
+++ public/asset/js/product-detail.js
@@ -0,0 +1,23 @@
+(function($){
+$(function(){
+
+    var
+    variants   = $('.product-options :input[name=size]'),
+    n11Urls    = $('.product-buy .n11-buy'),
+    radioValue = null,
+    activeUrl  = null,
+    i;
+
+    n11Urls.attr('target', '_blank');
+
+
+    var radioChanged = function() {
+        radioValue = variants.filter(':checked').val();
+        $(activeUrl).addClass('hidden');
+        activeUrl = n11Urls.filter('#url-'+radioValue).removeClass('hidden');
+    };
+
+    variants.change(radioChanged);
+
+});
+})(jQuery);
